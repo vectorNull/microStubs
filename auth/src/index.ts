4ec2408dc5a88938a32b53b37767a52c012b27a1
vectorNull <jose@vectornullsec.com>
2021-07-28 21:01:59 -0700

    Import auth routers and error-handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signupUserRouter } from "./routes/signup";
+import { signinUserRouter } from "./routes/signin";
+import { signoutUserRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(express.json());
 
@@ -7,25 +13,27 @@ app.use(express.json());
 // method: GET
 // body: -
 // route: /api/users/currentuser
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Response from GET route')
-})
+app.use(currentUserRouter);
 
 // Desc: Signup for account
 // method: POST
 // body: { email: string, password: string }
 // route: /api/users/signup
+app.use(signupUserRouter);
 
 // Desc: Sign in to an account
 // method: POST
 // body: { email: string, password: string }
 // route: /api/users/signin
+app.use(signinUserRouter);
 
 // Desc: Sign out of an account
 // method: POST
 // body: {}
 // route: /api/users/signout
+app.use(signoutUserRouter);
 
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Auth Service listening on port 3000");
