18f5fcc2c042acd1df1acb15d2019495516f36f2
vectorNull <jose@vectornullsec.com>
2021-07-30 18:12:09 -0700

    Import express-async-errors, NotFoundError handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
-
+import "express-async-errors";
 import { currentUserRouter } from "./routes/current-user";
 import { signupUserRouter } from "./routes/signup";
 import { signinUserRouter } from "./routes/signin";
 import { signoutUserRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
@@ -33,8 +34,11 @@ app.use(signinUserRouter);
 // route: /api/users/signout
 app.use(signoutUserRouter);
 
+app.all("*", async (req, res) => {
+	throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
-    console.log("Auth Service listening on port 3000");
+	console.log("Auth Service listening on port 3000");
 });
