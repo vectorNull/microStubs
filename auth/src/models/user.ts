696e76a9634917c2cd05582821e825f140de2fb6
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-02 20:20:42 -0700

    Add toJSON method to format response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { Password } from '../services/password'
+import { Password } from "../services/password";
 
 // An interface that describes the properties
 // that are requried to create a new User
@@ -21,24 +21,36 @@ interface UserDoc extends mongoose.Document {
 	password: string;
 }
 
-const userSchema = new mongoose.Schema<UserDoc>({
-	email: {
-		type: String,
-		required: true,
+const userSchema = new mongoose.Schema<UserDoc>(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
 	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		toJSON: {
+			transform(doc, ret) {
+				ret.id = ret._id;
+				delete ret._id;
+				delete ret.password;
+				delete ret.__v;
+			},
+		},
+	}
+);
 
-userSchema.pre('save', async function(done) {
-	if (this.isModified('password')) {
-		const hashed = await Password.toHash(this.get('password'))
-		this.set('password', hashed)
+userSchema.pre("save", async function (done) {
+	if (this.isModified("password")) {
+		const hashed = await Password.toHash(this.get("password"));
+		this.set("password", hashed);
 	}
 	done();
-})
+});
 
 userSchema.statics.build = (attrs: UserAttrs) => {
 	return new User(attrs);
