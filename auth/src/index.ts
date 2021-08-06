348bb3c848b7d6253ab739fdf6a26c4b750966dd
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-05 19:49:26 -0700

    Moved to app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,53 +1,6 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signupUserRouter } from "./routes/signup";
-import { signinUserRouter } from "./routes/signin";
-import { signoutUserRouter } from "./routes/signout";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); // traffic is being proxied through ingress-nginx
-app.use(express.json());
-app.use(
-	cookieSession({
-		signed: false, // maintaining cross-compatibility with other languages and frameworks
-		secure: true, // https
-	})
-);
-
-// Desc: Get the current user
-// method: GET
-// body: -
-// route: /api/users/currentuser
-app.use(currentUserRouter);
-
-// Desc: Signup for account
-// method: POST
-// body: { email: string, password: string }
-// route: /api/users/signup
-app.use(signupUserRouter);
-
-// Desc: Sign in to an account
-// method: POST
-// body: { email: string, password: string }
-// route: /api/users/signin
-app.use(signinUserRouter);
-
-// Desc: Sign out of an account
-// method: POST
-// body: {}
-// route: /api/users/signout
-app.use(signoutUserRouter);
-
-app.all("*", async (req, res) => {
-	throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app'
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
