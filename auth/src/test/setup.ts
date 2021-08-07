75f428fd092786b94f9b3dce6ef122dfb39b8b75
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-06 20:01:06 -0700

    Add local JWT_KEY for testing; refactor for MMS

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,8 +4,10 @@ import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
-	mongo = new MongoMemoryServer();
-	const mongoUri = await mongo.getUri();
+	process.env.JWT_KEY = "asdf";
+
+	mongo = await MongoMemoryServer.create();
+	const mongoUri = mongo.getUri();
 
 	await mongoose.connect(mongoUri, {
 		useNewUrlParser: true,
@@ -14,14 +16,14 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+	const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
-})
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
+});
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close()
-})
+	await mongo.stop();
+	await mongoose.connection.close();
+});
