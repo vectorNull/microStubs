1c08de847b0fbb809aa57f97eea2fa71daacb592
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-08 16:32:05 -0700

    Create global signin function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,15 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import { app } from "../app";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+	var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
-	process.env.JWT_KEY = "asdf";
+	process.env.JWT_KEY = 'asdf';
 
 	mongo = await MongoMemoryServer.create();
 	const mongoUri = mongo.getUri();
@@ -27,3 +32,20 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password,
+		})
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+
+	return cookie;
+};
