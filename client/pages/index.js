d8c4254ec9f62922a65c4145e8bbcc870cb15ff3
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-15 15:03:30 -0700

    Call getInitialProps with buildClient helper

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,17 @@
-const Landing = () => {
-	return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+    // console.log(currentUser);
+    // axios.get('/api/users/currentuser')
+    console.log(currentUser);
+	
+    return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data
+}
+
+export default LandingPage;
