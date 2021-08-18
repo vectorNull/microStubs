17f2fb97076a2e15c4f228f7f40c65c0707eee6d
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-17 22:58:27 -0700

    Final ticket test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/tickets'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app)
@@ -64,12 +65,22 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-    // add a check to make sure a ticket was saved
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+
+    const title = 'testTicket'
+
     await request(app)
         .post('/api/tickets')
+        .set('Cookie', global.signin())
         .send({
             title: 'testTicket',
             price: 10
         })
         .expect(201)
+    
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(10);
+    expect(tickets[0].title).toEqual(title);
 });
