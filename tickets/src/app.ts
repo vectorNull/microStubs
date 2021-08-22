38bc6817b304a40b1debeec66ace285a10b03077
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-21 17:46:10 -0700

    Add ticket routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 import {
 	errorHandler,
@@ -22,6 +25,9 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError();
