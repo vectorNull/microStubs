3f8d58b4771992272ee764c89eaf883a2b446e44
vectorNull <jose.rodriguez@prominentcode.com>
2021-08-24 23:17:15 -0700

    Add a queue group for alternating messages

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,7 @@ const stan = connect('tickets', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
 	console.log('Listener connected to NATS');
 
-	const subscription = stan.subscribe('ticket:created');
+	const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
 
 	subscription.on('message', (msg: Message) => {
 		const data = msg.getData();
